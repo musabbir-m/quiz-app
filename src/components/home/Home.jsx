b88d8f364ab2e81e57b49e15437f2e59628825ba
Musabbir-pr <musabbir313@gmail.com>
2023-05-02 15:30:42 +0600

    added categories

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Categories from './categories/Categories';
 
 const Home = () => {
     return (
         <div>
             <h1>This is home</h1>
+            <Categories></Categories>
         </div>
     );
 };
