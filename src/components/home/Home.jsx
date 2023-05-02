a7fba550f9c702999222e6cb60d1a24e3389316b
Musabbir-pr <musabbir313@gmail.com>
2023-05-02 15:55:51 +0600

    fixed merge conflict in Home.jsx

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,11 +1,30 @@
 import React from 'react';
+import Banner from './Banner';
+import AboutSection from './AboutSection';
+import TryItSection from './TryItSection';
+import EmailSection from './EmailSection';
+
+const Home = () => {
+    return (
+        <div>
+            <Banner></Banner>
+            <AboutSection></AboutSection>
+            <TryItSection></TryItSection>
+            <EmailSection></EmailSection>
+        </div>
+    );
+};
+import React from 'react';
+import Categories from './categories/Categories';
 
 const Home = () => {
     return (
         <div>
             <h1>This is home</h1>
+            <Categories></Categories>
         </div>
     );
 };
 
+export default Home;
 export default Home;
